865e4e1e65f2b654a630fb9e4a90df9326547cd4
silver1bear <juliusthefirst@gmail.com>
2019-12-06 18:55:26 +0100

    Initial commit. App completed.

---
+++ LightController/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace LightController
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Erforderliche Designervariable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Verwendete Ressourcen bereinigen.
+		/// </summary>
+		/// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		/// <summary>
+		/// Erforderliche Methode für die Designerunterstützung.
+		/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.socket1ONToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.socket1OFFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.socket2ONToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.socket2OFFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.socket3ONToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.socket3OFFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.socket4ONToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.socket4OFFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.contextMenuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// notifyIcon1
+			// 
+			this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+			this.notifyIcon1.BalloonTipText = "This is a tasktray app";
+			this.notifyIcon1.BalloonTipTitle = "This is the title";
+			this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+			this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+			this.notifyIcon1.Text = "notifyIcon1";
+			this.notifyIcon1.Visible = true;
+			this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+			// 
+			// contextMenuStrip1
+			// 
+			this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.socket1ONToolStripMenuItem,
+            this.socket1OFFToolStripMenuItem,
+            this.socket2ONToolStripMenuItem,
+            this.socket2OFFToolStripMenuItem,
+            this.socket3ONToolStripMenuItem,
+            this.socket3OFFToolStripMenuItem,
+            this.socket4ONToolStripMenuItem,
+            this.socket4OFFToolStripMenuItem});
+			this.contextMenuStrip1.Name = "contextMenuStrip1";
+			this.contextMenuStrip1.Size = new System.Drawing.Size(143, 180);
+			// 
+			// socket1ONToolStripMenuItem
+			// 
+			this.socket1ONToolStripMenuItem.Name = "socket1ONToolStripMenuItem";
+			this.socket1ONToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+			this.socket1ONToolStripMenuItem.Text = "Socket 1 ON";
+			// 
+			// socket1OFFToolStripMenuItem
+			// 
+			this.socket1OFFToolStripMenuItem.Name = "socket1OFFToolStripMenuItem";
+			this.socket1OFFToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+			this.socket1OFFToolStripMenuItem.Text = "Socket 1 OFF";
+			// 
+			// socket2ONToolStripMenuItem
+			// 
+			this.socket2ONToolStripMenuItem.Name = "socket2ONToolStripMenuItem";
+			this.socket2ONToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+			this.socket2ONToolStripMenuItem.Text = "Socket 2 ON";
+			// 
+			// socket2OFFToolStripMenuItem
+			// 
+			this.socket2OFFToolStripMenuItem.Name = "socket2OFFToolStripMenuItem";
+			this.socket2OFFToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+			this.socket2OFFToolStripMenuItem.Text = "Socket 2 OFF";
+			// 
+			// socket3ONToolStripMenuItem
+			// 
+			this.socket3ONToolStripMenuItem.Name = "socket3ONToolStripMenuItem";
+			this.socket3ONToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+			this.socket3ONToolStripMenuItem.Text = "Socket 3 ON";
+			// 
+			// socket3OFFToolStripMenuItem
+			// 
+			this.socket3OFFToolStripMenuItem.Name = "socket3OFFToolStripMenuItem";
+			this.socket3OFFToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+			this.socket3OFFToolStripMenuItem.Text = "Socket 3 OFF";
+			// 
+			// socket4ONToolStripMenuItem
+			// 
+			this.socket4ONToolStripMenuItem.Name = "socket4ONToolStripMenuItem";
+			this.socket4ONToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+			this.socket4ONToolStripMenuItem.Text = "Socket 4 ON";
+			// 
+			// socket4OFFToolStripMenuItem
+			// 
+			this.socket4OFFToolStripMenuItem.Name = "socket4OFFToolStripMenuItem";
+			this.socket4OFFToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+			this.socket4OFFToolStripMenuItem.Text = "Socket 4 OFF";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.contextMenuStrip1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		private System.Windows.Forms.NotifyIcon notifyIcon1;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem socket1ONToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem socket1OFFToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem socket2ONToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem socket2OFFToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem socket3ONToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem socket3OFFToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem socket4ONToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem socket4OFFToolStripMenuItem;
+	}
+}
+
---
+++ LightController/SystemTrayApplication.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+
+namespace LightController
+{
+	class SystemTrayApplication : ApplicationContext
+	{
+		private NotifyIcon	trayIcon;
+		private ContextMenu socketOverview;
+
+		public SystemTrayApplication()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+
+			socketOverview = new ContextMenu();
+			socketOverview.MenuItems.Add(new MenuItem("Socket 1 ON",	(sender, args) => ControlSocket(0, 1)));
+			socketOverview.MenuItems.Add(new MenuItem("Socket 1 OFF",	(sender, args) => ControlSocket(0, 0)));
+			socketOverview.MenuItems.Add(new MenuItem("Socket 2 ON",	(sender, args) => ControlSocket(1, 1)));
+			socketOverview.MenuItems.Add(new MenuItem("Socket 2 OFF",	(sender, args) => ControlSocket(1, 0)));
+			socketOverview.MenuItems.Add(new MenuItem("Socket 3 ON",	(sender, args) => ControlSocket(2, 1)));
+			socketOverview.MenuItems.Add(new MenuItem("Socket 3 OFF",	(sender, args) => ControlSocket(2, 0)));
+			socketOverview.MenuItems.Add(new MenuItem("Socket 4 ON",	(sender, args) => ControlSocket(3, 1)));
+			socketOverview.MenuItems.Add(new MenuItem("Socket 4 OFF",	(sender, args) => ControlSocket(3, 0)));
+			socketOverview.MenuItems.Add(new MenuItem("-"));
+			socketOverview.MenuItems.Add(new MenuItem("All ON",			(sender, args) => ControlSocket(-1, 1)));
+			socketOverview.MenuItems.Add(new MenuItem("All OFF",		(sender, args) => ControlSocket(-1, 0)));
+			socketOverview.MenuItems.Add(new MenuItem("-"));
+			socketOverview.MenuItems.Add(new MenuItem("Exit",			(sender, args) => Exit(sender, args)));
+
+			trayIcon = new NotifyIcon()
+			{
+				Visible = true,
+				Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon"))),
+				ContextMenu = socketOverview
+			};
+		}
+
+		static async Task SendRequest(int socketNo, int IO)
+		{
+			if (socketNo == -1)
+			{
+				using (var client = new HttpClient())
+				{
+					var msg = new HttpRequestMessage(HttpMethod.Get, "http://raspberrypi:6969/?action=" + ((IO == 1) ? "turnon" : "turnoff"));
+					await client.SendAsync(msg);
+				}
+			}
+			else
+			{
+				using (var client = new HttpClient())
+				{
+					var msg = new HttpRequestMessage(HttpMethod.Get, "http://raspberrypi:6969/?action=" + ((IO == 1) ? "on" : "off") + "&no=" + socketNo);
+					await client.SendAsync(msg);
+				}
+			}
+		}
+
+		//[System.Runtime.InteropServices.ComVisible(true)]
+		//[System.Serializable]
+		void ControlSocket(int socketNo, int IO)
+		{
+			Task task = SendRequest(socketNo, IO);
+			task.Wait();
+		}
+
+		void Exit(object Sender, EventArgs e)
+		{
+			trayIcon.Visible = false;
+			Application.Exit();
+		}
+
+	}
+}
